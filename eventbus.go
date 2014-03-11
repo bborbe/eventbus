13f605f8036c93709169b071a071bcf7757dca16
Benjamin Borbe <bborbe@rocketnews.de>
2014-03-11 13:25:31 +0100

    add basic eventbus

---
+++ eventbus.go
@@ -0,0 +1,71 @@
+package eventbus
+
+import (
+	"reflect"
+	"fmt"
+	"sync"
+)
+
+type EventBus interface {
+	RegisterHandler(fn interface {}) error
+	UnregisterHandler(fn interface {}) error
+	Publish(event interface{}) error
+}
+
+type eventBus struct {
+	handlers map[reflect.Type][]reflect.Value
+	lock     sync.RWMutex
+}
+
+func New() *eventBus {
+	e := new(eventBus)
+	e.handlers = make(map[reflect.Type][]reflect.Value)
+	return e
+}
+
+func (e *eventBus) RegisterHandler(fn interface {}) error {
+	e.lock.Lock()
+	defer e.lock.Unlock()
+
+	v := reflect.ValueOf(fn)
+	def := v.Type()
+	if def.NumIn() != 1 {
+		return fmt.Errorf("Handler must have a single argument")
+	}
+	argument := def.In(0)
+	e.handlers[argument] = append(e.handlers[argument], v)
+	return nil
+}
+
+func (e *eventBus) UnregisterHandler(fn interface {}) error {
+	e.lock.Lock()
+	defer e.lock.Unlock()
+
+	v := reflect.ValueOf(fn)
+	def := v.Type()
+	if def.NumIn() != 1 {
+		return fmt.Errorf("Handler must have a single argument")
+	}
+	argument := def.In(0)
+	handlers := make([]reflect.Value, 0)
+	for _, handler := range e.handlers[argument] {
+		if handler != v {
+			handlers = append(handlers, handler)
+		}
+	}
+	e.handlers[argument] = handlers
+	return nil
+}
+
+func (e *eventBus) Publish(event interface{}) error {
+	e.lock.RLock()
+	defer e.lock.RUnlock()
+
+	t := reflect.TypeOf(event)
+	args := [...]reflect.Value{reflect.ValueOf(event)}
+	fns := e.handlers[t]
+	for _, fn := range fns {
+		fn.Call(args[:])
+	}
+	return nil
+}
---
+++ eventbus_test.go
@@ -0,0 +1,103 @@
+package eventbus
+
+import (
+	. "github.com/bborbe/assert"
+	"testing"
+)
+
+func TestImplements(t *testing.T) {
+	eventBus := New()
+	var i *EventBus
+	err := AssertThat(eventBus, Implements(i).Message("check type"))
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+type TestEventA struct {}
+type TestEventB struct {}
+
+func TestPublishHandlerIsCalledIfTypeIsMatching(t *testing.T) {
+	called := false
+	eventBus := New()
+	eventBus.RegisterHandler(func(event TestEventA) {
+		called = true
+	})
+	eventBus.Publish(TestEventA{})
+	err := AssertThat(called, Is(true))
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestPublisMultiHandlerAreCalled(t *testing.T) {
+	counter := 0
+	eventBus := New()
+	eventBus.RegisterHandler(func(event TestEventA) {
+		counter++
+	})
+	eventBus.RegisterHandler(func(event TestEventA) {
+		counter++
+	})
+	eventBus.Publish(TestEventA{})
+	err := AssertThat(counter, Is(2))
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestPublishHandlerIsNotCalledIfTypeIsNotMatching(t *testing.T) {
+	called := false
+	eventBus := New()
+	eventBus.RegisterHandler(func(event TestEventA) {
+		called = true
+	})
+	eventBus.Publish(TestEventB{})
+	err := AssertThat(called, Is(false))
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestRegisterHandlerReturnErrorFuncWithoutArgs(t *testing.T) {
+	eventBus := New()
+	err := eventBus.RegisterHandler(func() {})
+	err = AssertThat(err, NotNilValue())
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestRegisterHandlerReturnErrorFuncWithTwoArgs(t *testing.T) {
+	eventBus := New()
+	err := eventBus.RegisterHandler(func(a TestEventA, b TestEventA) {})
+	err = AssertThat(err, NotNilValue())
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestRegisterHandlerReturnNoErrorFuncWithOneArg(t *testing.T) {
+	eventBus := New()
+	err := eventBus.RegisterHandler(func(event TestEventA) {})
+	err = AssertThat(err, NilValue())
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestUnregisterHandler(t *testing.T) {
+	counter := 0
+	eventBus := New()
+	fn := func(event TestEventA) {
+		counter++
+	}
+	eventBus.RegisterHandler(fn)
+	eventBus.Publish(TestEventA{})
+	eventBus.UnregisterHandler(fn)
+	eventBus.Publish(TestEventA{})
+	err := AssertThat(counter, Is(1))
+	if err != nil {
+		t.Fatal(err)
+	}
+}
